69cc0af20bcdeb83c1140e50e5fa0f484cd8a410
Syed <dcsyedfaraz@gmail.com>
2024-04-24 23:56:42 +0500

    Update resources/views/cookie.blade.php: Format improvements and clarified cookie usage
    This commit updates the cookie policy view file with formatting adjustments and more explicit explanations of how cookies are used, including enhancing user experience

--- resources/views/cookie.blade.php
+++ resources/views/cookie.blade.php
@@ -14,38 +14,38 @@
                This Cookie Policy explains how we use cookies and similar technologies to enhance your experience on our website while respecting your privacy. By using our website, you consent to the use of cookies in accordance with this policy.<br><br>
 
                 <strong>WHAT ARE COOKIES?</strong><br>
-                
+
                 Cookies are small pieces of data stored on your device (computer or mobile device) when you visit a website. They are widely used to make websites work more efficiently and to provide information to website owners.<br><br>
-                
+
                 <h4>HOW WE USE COOKIES</h4><br>
-                
+
                 We use cookies for the following purposes:<br><br>
-                
+
                 <strong>ENHANCING USER EXPERIENCE:</strong><br>
                 Cookies help us provide you with a personalized experience by remembering your preferences and settings. For example, they remember your login details, language preferences, and display settings.<br><br>
-                
+
                 <strong>ANALYZING WEBSITE PERFORMANCE: </strong><br>
                 We use cookies to analyze how visitors use our website, which helps us improve its performance and functionality. These cookies collect anonymous information about visitor behavior, such as the number of visitors to the website and the pages they visit most often.<br><br>
-                
+
                 <strong>THIRD PARTY INTEGRATIONS:</strong> <br>
                 Some cookies may be placed by third-party services that appear on our website. These cookies are used for purposes such as analyzing website traffic, delivering targeted advertisements, and providing social media features.<br><br>
-                
+
                 <strong>YOUR PRIVACY:</strong><br>
-                
+
                 We respect your privacy and are committed to protecting your personal information. We do not sell, trade, or otherwise transfer your personal information to third parties without your consent. Any information collected through cookies is used solely for the purposes outlined in this policy.<br><br>
-                
+
                 <strong>MANAGING COOKIES:</strong><br>
-                
+
                 You can control and manage cookies in various ways. Most web browsers allow you to accept or reject cookies, delete existing cookies, and set preferences for future cookie usage. Please note that disabling cookies may affect your experience on our website and limit certain functionalities.
                 <br><br>
-                
-                
+
+
                 <strong>CHANGES TO THIS POLICY</strong><br>
-                
+
                 We may update this Cookie Policy from time to time to reflect changes in our practices or legal requirements. We encourage you to review this policy periodically for any updates or changes.<br><br>
-                
+
                 <strong>CONTACT US</strong><br>
-                
+
                 If you have any questions or concerns about our use of cookies or this Cookie Policy, please contact us at parkshadowshomeowners@gmail.com<br><br>
 
             </p>
