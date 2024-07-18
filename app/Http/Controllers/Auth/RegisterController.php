04d9af0aeafd63a51e9c29d6e328d3b25bc1a440
Syed <dcsyedfaraz@gmail.com>
2024-07-19 00:10:42 +0500

    "Relaxed phone number and image validation rules in RegisterController"

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,13 +63,13 @@ class RegisterController extends Controller
             'username' => ['regex:/^[^\s]+$/', 'required', 'string', 'max:15', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'same:confirm_password'],
             'position' => 'required|string|in:rent,owner',
-            'phone' => 'required|string|regex:/^\+1 \(\d{3}\) \d{3}-\d{4}$/',
+            'phone' => 'required|string',
             'landlord_name' => 'nullable|required_if:position,rent|string|max:255',
-            'landlord_phone_number' => 'nullable|required_if:position,rent|string|regex:/^\+1 \(\d{3}\) \d{3}-\d{4}$/',
+            'landlord_phone_number' => 'nullable|required_if:position,rent|string',
             'landlord_email_address' => 'nullable|required_if:position,rent|string|email|max:255',
             'landlord_address' => 'nullable|required_if:position,rent|string|max:255',
             'address' => 'required|string|max:255',
-            'image' => 'nullable|image|max:2048',
+            'image' => 'nullable|max:2048',
             'date' => 'nullable|required_if:position,owner|date_format:Y-m-d',
         ]);
         if ($validatedData->fails()) {
