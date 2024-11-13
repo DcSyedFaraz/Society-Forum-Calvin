678a314cb600b55cf269e19d645bc9e9f85d4f7b
Syed <dcsyedfaraz@gmail.com>
2024-11-13 23:33:24 +0500

    "Added modal window to display user details on admin dashboard"

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -197,6 +197,155 @@
                                                         data-bs-target="#exampleModal{{ $key }}">
                                                         View
                                                     </button>
+                                                    <!-- Modal -->
+                                                    <div class="modal fade" id="exampleModal{{ $key }}"
+                                                        tabindex="-1" aria-labelledby="exampleModalLabel"
+                                                        aria-hidden="true">
+                                                        <div class="modal-dialog">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">
+                                                                        Name:{{ $user->name }}</h5>
+                                                                    <button type="button" class="btn-close"
+                                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    @if ($user->image)
+                                                                        <img class="rounded"
+                                                                            src="{{ asset('storage/images/' . $user->image) }}"
+                                                                            alt="Image" width="100%" height="auto">
+                                                                    @endif
+                                                                    @if ($user->roles->isNotEmpty())
+                                                                        <p class="mt-3"><strong
+                                                                                class="me-2">Role:</strong>
+                                                                            <span
+                                                                                class="badge bg-primary text-uppercase">{{ $user->roles->first()->name }}</span>
+                                                                        </p>
+                                                                    @else
+                                                                        <p class="mt-3"><strong
+                                                                                class="me-2">Role:</strong>
+                                                                            <span
+                                                                                class="badge bg-secondary text-uppercase">No
+                                                                                Role
+                                                                                Assigned</span>
+                                                                        </p>
+                                                                    @endif
+                                                                    <p class="mt-3"><strong class="me-2">User
+                                                                            Name:</strong>
+                                                                        <span
+                                                                            class="badge bg-primary">{{ $user->username }}</span>
+                                                                    </p>
+                                                                    @if ($user->member)
+                                                                        <div class="user-info">
+                                                                            <p class="text-wrap">
+                                                                                <strong class="me-2">Position:</strong>
+                                                                                <span
+                                                                                    class="badge text-uppercase bg-success">{{ ucfirst($user->member->position) }}</span>
+                                                                            </p>
+
+                                                                            <p class="text-wrap">
+                                                                                <strong class="me-2">Address:</strong>
+                                                                                {{ $user->member->address }}
+                                                                            </p>
+
+                                                                            @if ($user->member->position === 'rent')
+                                                                                <!-- Fields specific to Renters -->
+                                                                                <p class="text-wrap">
+                                                                                    <strong class="me-2">Landlord
+                                                                                        Address:</strong>
+                                                                                    {{ $user->member->landlord_address ?? 'N/A' }}
+                                                                                </p>
+                                                                                <p class="text-wrap">
+                                                                                    <strong class="me-2">Landlord
+                                                                                        Name:</strong>
+                                                                                    {{ $user->member->landlord_name ?? 'N/A' }}
+                                                                                </p>
+                                                                                <p class="text-wrap">
+                                                                                    <strong class="me-2">Landlord Phone
+                                                                                        Number:</strong>
+                                                                                    {{ $user->member->landlord_phone_number ?? 'N/A' }}
+                                                                                </p>
+                                                                                <p class="text-wrap">
+                                                                                    <strong class="me-2">Landlord Email
+                                                                                        Address:</strong>
+                                                                                    {{ $user->member->landlord_email_address ?? 'N/A' }}
+                                                                                </p>
+                                                                            @elseif ($user->member->position === 'owner')
+                                                                                <!-- Fields specific to Owners -->
+                                                                                <p class="text-wrap">
+                                                                                    <strong class="me-2">Date of
+                                                                                        Purchase:</strong>
+                                                                                    {{ $user->member->date_of_purchase ?? 'N/A' }}
+                                                                                </p>
+                                                                                <p class="text-wrap">
+                                                                                    <strong class="me-2">Park Shadows
+                                                                                        Property
+                                                                                        Address:</strong>
+                                                                                    {{ $user->member->park_address ?? 'N/A' }}
+                                                                                </p>
+                                                                            @endif
+                                                                        </div>
+                                                                    @elseif ($user->executive)
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Mailing
+                                                                                Address:</strong>
+                                                                            {{ $user->executive->address ?? 'N/A' }}
+                                                                        </p>
+                                                                        <p class="text-wrap"><strong class="me-2">Park
+                                                                                Address:</strong>
+                                                                            {{ $user->executive->parkaddress ?? 'N/A' }}
+                                                                        </p>
+                                                                        <p class="text-wrap"><strong class="me-2">H.O.A
+                                                                                Address:</strong>
+                                                                            {{ $user->executive->hoaaddress ?? 'N/A' }}
+                                                                        </p>
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Designation:</strong>
+                                                                            {{ $user->executive->designation ?? 'N/A' }}
+                                                                        </p>
+                                                                    @elseif ($user->agent)
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">License:</strong>
+                                                                            {{ $user->agent->license }}</p>
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Company
+                                                                                Name:</strong>
+                                                                            {{ $user->agent->company_name }}</p>
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Physical
+                                                                                Address:</strong>
+                                                                            {{ $user->agent->physical_address }}</p>
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Company
+                                                                                Mailing Address:</strong>
+                                                                            {{ $user->agent->company_mailing_address }}</p>
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Company
+                                                                                Phone Number:</strong>
+                                                                            {{ $user->agent->company_phone_number }}</p>
+                                                                        <p class="text-wrap"><strong
+                                                                                class="me-2">Company
+                                                                                Email:</strong>
+                                                                            {{ $user->agent->company_email }}</p>
+                                                                        @if ($user->agent->company_website)
+                                                                            <p class="text-wrap"><strong
+                                                                                    class="me-2">Company
+                                                                                    Website:</strong> <a
+                                                                                    href="{{ $user->agent->company_website }}">{{ $user->agent->company_website }}</a>
+                                                                            </p>
+                                                                        @endif
+                                                                    @else
+                                                                        <p class="text-wrap">No user details available.</p>
+                                                                    @endif
+
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-secondary"
+                                                                        data-bs-dismiss="modal">Close</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </td>
                                                 <td>
                                                     @if ($user->access == 'declined')
