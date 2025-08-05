cb4cfa444e5e19f67df609029fe1abb8c156c7d8
Syed Faraz Shahid <135306876+DcSyedFaraz@users.noreply.github.com>
2025-08-05 18:08:10 +0500

    Handle array inputs in file manager helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use Request;
 use Illuminate\Support\Facades\View;
 use App\Models\Category;
 use App\Models\GeneralSetting;
+use App\Support\CustomLfm;
+use UniSharp\LaravelFilemanager\Lfm;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Override the default Filemanager helper so we can customize
+        // how request input is translated. The stock implementation
+        // fails when an array is passed (e.g. moving multiple files).
+        // Binding our extension ensures all package controllers use it.
+        $this->app->bind(Lfm::class, function ($app) {
+            return new CustomLfm($app['config'], $app['request']);
+        });
     }
 
     /**
